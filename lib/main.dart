088b1eec5f43ee7b991c7812b6b8a2c8e0cdf3a2
Rayhan Abduhuda <rayrayhan920@gmail.com>
2023-10-20 21:36:09 +0700

    lanjut

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,20 @@ import 'package:second_app/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 3, 104, 30),
+                  Color.fromARGB(255, 16, 231, 73),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: const StartScreen()),
       ),
     ),
   );
