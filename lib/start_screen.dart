14c79be9166a2c8c5c025fadde519f79c2e5f63b
Rayhan Abduhuda <rayrayhan920@gmail.com>
2023-10-21 13:38:20 +0700

    opacity

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -9,10 +9,18 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset(
-            'assets/images/quiz-logo.png',
-            width: 300,
-          ),
+            Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 300,
+              color: const Color.fromARGB(150, 255, 255, 255),
+            ),
+          // Opacity (
+          //   opacity: 0.3,
+          //   child: Image.asset(
+          //     'assets/images/quiz-logo.png',
+          //     width: 300,
+          //   ),
+          // ),
           const SizedBox(height: 80),
           const Text(
             'Ayo quiz bersama',
