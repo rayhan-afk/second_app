cd591319563a3ba0ce4f70d3a41a8474da38af5a
Rayhan Abduhuda <rayrayhan920@gmail.com>
2023-10-23 13:15:03 +0700

    question 2

--- lib/data/questions.dart
+++ lib/data/questions.dart
@@ -1,13 +1,54 @@
 import 'package:second_app/models/quiz_question.dart';
 
 const question = [
+  QuizQuestion('Apa rasa susu kesukaan alittria?',
+    [
+      'Strawberry',
+      'Coklat',
+      'Vanilla',
+      'Mangga',
+    ],
+  ),
+  QuizQuestion('Kapan tanggal lahir alittria?', [
+    'By combining widgets in code',
+    'By combining widgets in a visual editor',
+    'By defining widgets in config files',
+    'By using XCode for iOS and Android Studio for Android',
+  ]),
   QuizQuestion(
-    'Apa susu kesukaan alitt?', 
-  [
-    'Strawberry',
-    'Coklat',
-    'Vanilla',
-    'Mangga',
-  ],
-  )
+    'Apa genre film favorit alittria?',
+    [
+      'Update UI as data changes',
+      'Update data as UI changes',
+      'Ignore data changes',
+      'Render UI that does not depend on data',
+    ],
+  ),
+  QuizQuestion(
+    'Apa makanan favorit alittria?',
+    [
+      'StatelessWidget',
+      'StatefulWidget',
+      'Both are equally good',
+      'None of the above',
+    ],
+  ),
+  QuizQuestion(
+    'Apa warna kesukaan alittria?',
+    [
+      'The UI is not updated',
+      'The UI is updated',
+      'The closest StatefulWidget is updated',
+      'Any nested StatefulWidgets are updated',
+    ],
+  ),
+  QuizQuestion(
+    'Siapa orang favorit alittria?',
+    [
+      'Rayhan Abduhuda',
+      'Ray',
+      'Rayhan',
+      'Abduhuda',
+    ],
+  ),
 ];
