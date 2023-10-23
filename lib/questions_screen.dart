dd30fd2ea3e3e894c699d2f552a6903db7e37da3
Rayhan Abduhuda <rayrayhan920@gmail.com>
2023-10-23 13:30:54 +0700

    question screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,6 +12,27 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-   return const Text('QuestionScreen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Pertanyaan...'),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3'),
+          ),
+        ],
+      ),
+    );
   }
 }
