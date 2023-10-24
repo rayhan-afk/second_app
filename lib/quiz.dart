13cf46cdf2aa3485cdb0788542e58a8c78cdec7b
Rayhan Abduhuda <rayrayhan920@gmail.com>
2023-10-24 22:12:35 +0700

    results screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:second_app/questions_screen.dart';
 import 'package:second_app/start_screen.dart';
 import 'package:second_app/data/questions.dart';
+import 'package:second_app/results_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -28,7 +29,7 @@ class _QuizState extends State<Quiz> {
     if (selectedAnswers.length == questions.length) {
       setState(() {
         selectedAnswers = [];
-        activeScreen = 'start-screen';
+        activeScreen = 'results-screen';
       });
     }
   }
@@ -40,6 +41,12 @@ class _QuizState extends State<Quiz> {
     if (activeScreen == 'question-screen') {
       screenWidget = QuestionsScreen(
         onSelectAnswer: chooseAnswer,
+      );
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
       );
     }
 
---
+++ lib/results_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({super.key, required this.chosenAnswers});
+
+  final List<String> chosenAnswers;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Kamu menjawab X dari Y pertanyaan'),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text('List dari jawaban dan pertanyaan...'),
+            const SizedBox(
+              height: 30,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Restart Quiz!'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
